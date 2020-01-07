e5fddd6304cd1a760a929452df963f0ed83cb03f
Suparna Roy <suparnadroy@yahoo.com>
2020-01-07 07:38:04 +0530

    Local user configured now and changes made to product and store model

--- product.rb
+++ product.rb
@@ -1,2 +1,3 @@
 Product fix
 suroy adding changes to the product model
+changes to be made using local user suroy
--- store.rb
+++ store.rb
@@ -1,2 +1,3 @@
 Bug fix
 suroy adding changes to the store model
+changes to be made using local user suroy
